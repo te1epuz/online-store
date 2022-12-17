64417d735e46f173a1f0abfa7479e14d3b3e9f15
VictorM <neuron96@mail.ru>
2022-12-18 00:24:12 +0400

    feat: feat homepage

--- src/App/types/types.ts
+++ src/App/types/types.ts
@@ -1,3 +1,21 @@
 /// types
 
-export type {}
+type TResponse = {
+  products: TProduct[];
+};
+
+type TProduct = {
+  id: number | string;
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  images: string[];
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+  title: string;
+};
+
+export type { TProduct, TResponse };
