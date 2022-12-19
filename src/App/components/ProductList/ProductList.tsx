92d2df12a6687aaab98fde5970418b2d0ae43741
VictorM <neuron96@mail.ru>
2022-12-19 20:13:48 +0500

    feat: feat filter

--- src/App/components/ProductList/ProductList.tsx
+++ src/App/components/ProductList/ProductList.tsx
@@ -1,14 +1,33 @@
+// import React from 'react';
 import { useAsyncValue } from 'react-router-dom';
 import { TProduct } from '../../types/types';
 import { ProductCard } from '../ProductCard/ProductCard';
 
-function ProductList({ category }: { category: string | null }) {
+type TProps = {
+  categories: string[];
+  brands: string[];
+};
+
+function ProductList({ categories, brands }: TProps) {
   const products = useAsyncValue() as TProduct[];
   const filteredArr = products.filter((i) => {
-    if (!category) return i;
-    return i.category === category;
+    if (categories?.length && brands.length) {
+      return (
+        categories.some((j) => j.toLowerCase() === i.category.toLowerCase()) &&
+        brands.some((j) => j.toLowerCase() === i.brand.toLowerCase())
+      );
+    }
+
+    if (categories?.length) return categories.some((j) => j.toLowerCase() === i.category.toLowerCase());
+    if (brands.length) return brands.some((j) => j.toLowerCase() === i.brand.toLowerCase());
+
+    return i;
   });
+
   console.log(filteredArr);
+  console.log(categories);
+  console.log(brands);
+  if (!filteredArr.length) return <h1>No products found</h1>;
   return (
     <>
       {filteredArr.map((item) => (
